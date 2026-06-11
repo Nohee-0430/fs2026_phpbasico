1b0696834b2e0b9d0e29a33eb6c2367c8be5c677
Nohelia Barillas <noheliabarillas52@gmail.com>
2026-06-11 09:52:30 -0600

    Solución 5

---
+++ clases/Solucion5.php
@@ -0,0 +1,38 @@
+<?php
+    class Solucion5{
+        private $valor1;
+        private $valor2;
+
+        public function setValor1 ($valor1){
+            $this->valor1 = $valor1; 
+        }
+        public function setValor2 ($valor2){
+            $this->valor2 = $valor2; 
+        }
+        public function generarNumeros(){
+            $mayor=0;
+            /* esta condicion cambia los valores para que el ciclo funcione
+            sin importar cual valor es mayor*/
+            if($this->valor1 > $this->valor2){
+                $mayor=$this->valor1;
+                $this->valor1=$this->valor2;
+                $this->valor2=$mayor;
+            }
+            echo "<br>Los números pares son:";
+            for ($i= $this->valor1; $i <= $this->valor2; $i++) { 
+                if ($i % 2 == 0) {
+                    echo "<br>$i";
+                }
+            }
+        }
+        public function verificarMayorMenor(){
+            if($this->valo1 > $this->valor2){
+                echo "<br> $this->valor1 es mayor que $this->valor2";
+                Jelseif ($this->valo2 > $this->valor1) {
+                qcho "<br> $this->valor2 es mayor que $this->valor1";
+            }else {
+                echo "<br> Los numeros son iguales";
+            }
+            }
+    }
+?>
---
+++ form_solucion5.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Problema No. 5</h1>
+    </header>
+    <main>
+        <p>
+
+        </p>
+        <form action="objetos/objetoSolucion5.php" method="post">
+            <label for="num_valor1">Primer valor</label>
+            <input type="number" name="num_valor1" id="num_valor1">
+            <label for="num_valor2">Segundo valor</label>
+            <input type="number" name="num_valor2" id="num_valor2">
+            <button type="submit">Generar números</button>
+        </form>
+    </main>
+</body>
+</html>
